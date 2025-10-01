37c0c554721c43bde9c5accb718744f80df39f52
SS Karan <karanss.m05@gmail.com>
2025-10-02 00:15:54 +0530

    Time: 3 ms (63.49%) | Memory: 9.2 MB (78.68%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -3,20 +3,18 @@ public:
     string intToRoman(int num) {
 
 
-    vector<pair<int, string>> valueSymbols{
-        {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
-        {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
-        {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}
-    };
+    int values[]   = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    string symbols[] = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
 
     string res = "";
 
-    for (auto &[value, symbol] : valueSymbols) {
-        int count = num / value;   
-        num %= value;              // remaining number
-        for (int i = 0; i < count; i++)
-            res += symbol;
+    for (int i = 0; i < 13; i++) {
+        int count = num / values[i];   // time of iteration 
+        num %= values[i];              // remaining number
+        while (count--) res += symbols[i]; // count -- in notes its shorter way to write it
     }
+
+
 
     return res;
 }
