f52179787b581d7a0fc60651688f7ed0dd6a59d3
SS Karan <karanss.m05@gmail.com>
2026-01-29 00:35:57 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (99.83%) - LeetSync

---
+++ 32-longest-valid-parentheses/longest-valid-parentheses.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int left = 0, right = 0, maxLength = 0;
+
+        // Left to right traversal
+        for (int i = 0; i < (int)s.length(); i++) {
+            if (s[i] == '(') left++;
+            else right++;
+
+            if (left == right) {
+                maxLength = max(maxLength, 2 * right);
+            } else if (right > left) {
+                left = right = 0; // Reset counters
+            }
+        }
+
+        left = right = 0;
+
+        // Right to left traversal
+        for (int i = (int)s.length() - 1; i >= 0; i--) {
+            if (s[i] == '(') left++;
+            else right++;
+
+            if (left == right) {
+                maxLength = max(maxLength, 2 * left);
+            } else if (left > right) {
+                left = right = 0; // Reset counters
+            }
+        }
+
+        return maxLength;
+    }
+};
